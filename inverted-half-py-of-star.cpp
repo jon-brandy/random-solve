36b55e63b4c3d444c5cb881977220fba6740f1b9
Nicolas Saputra Gunawan <70703371+Brandy969@users.noreply.github.com>
2021-09-26 14:03:03 +0700

    Create inverted-half-py-of-star.cpp

---
+++ inverted-half-py-of-star.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(int i = num; i >= 1; i--)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
